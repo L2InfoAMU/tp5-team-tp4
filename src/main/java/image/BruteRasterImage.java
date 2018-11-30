a8a534dea0407ea26ab8b22a6c966bc0b78b7476
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2018-11-30 09:26:01 +0100

    Creation des constructeurs

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -2,6 +2,25 @@ package image;
 
 import java.awt.*;
 
-public class BruteRasterImage {
-   Color color;
+public class BruteRasterImage implements Image{
+   Color[][] colors;
+    int width;
+    int height;
+
+    public BruteRasterImage(Color color, int width, int height){
+        this.width = width;
+        this.height = height;
+        colors = new Color[width][height];
+        for(int i = 0; i < width; i++)
+            for(int j = 0; j < height; j++)
+                colors[width][height] = color;
+    }
+
+    public BruteRasterImage(Color[][] colors){
+        this.colors = colors;
+        width = colors.length;
+        height = colors[0].length;
+    }
+
+    public void creatRepresentation()
 }
