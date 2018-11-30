ebc7e931bed9c8de46f9d92ab3a0c69459a33cc4
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2018-11-30 09:48:09 +0100

    setPixerlColor done setPixerlColor done

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -2,38 +2,47 @@ package image;
 
 import java.awt.*;
 
-public class BruteRasterImage implements Image{
-   Color[][] colors;
+public class BruteRasterImage implements Image {
+    Color[][] colors;
     int width;
     int height;
 
-    public BruteRasterImage(Color color, int width, int height){
+    public BruteRasterImage(Color color, int width, int height) {
         this.width = width;
         this.height = height;
         colors = new Color[width][height];
-        for(int i = 0; i < width; i++)
-            for(int j = 0; j < height; j++)
-                colors[width][height] = color;
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                colors[i][j] = color;
     }
 
-    public BruteRasterImage(Color[][] colors){
+    public BruteRasterImage(Color[][] colors) {
         this.colors = colors;
         width = colors.length;
         height = colors[0].length;
     }
 
-    public void creatRepresentation(){
+    public void creatRepresentation() {
     }
 
-    public void setPixelColor(Color color, int x, int y){
+    public void setPixelColor(Color color, int x, int y) {
         colors[x][y] = color;
     }
 
-    public Color getPixelColor(int x, int y){
+    public Color getPixelColor(int x, int y) {
         return colors[x][y];
     }
 
-    private void setPixelsColor(Color[][] pixels){
+    private void setPixelsColor(Color[][] pixels) {
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                colors[i][j] = pixels[i][j];
 
+    }
+
+    private void setPixelsColor(Color color) {
+        for (int i = 0; i < width; i++)
+            for (int j = 0; j < height; j++)
+                colors[i][j] = color;
     }
 }
