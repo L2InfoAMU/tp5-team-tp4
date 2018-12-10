5519b4b1b143fabc92888531050b82183811c53a
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2018-12-10 20:19:05 +0100

    Création de la classe RasterImage

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -5,14 +5,14 @@ import javafx.scene.paint.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PaletteRasterImage implements Image{
+public class PaletteRasterImage implements Image {
 
     List<Color> palette;
     int[][] indexesOfColors;
     int width;
     int height;
 
-    public PaletteRasterImage(Color color, int width, int height){
+    public PaletteRasterImage(Color color, int width, int height) {
         this.width = width;
         this.height = height;
         palette = new ArrayList<>();
@@ -20,65 +20,66 @@ public class PaletteRasterImage implements Image{
         palette.add(color);
         for (int i = 0; i < width; i++)
             for (int j = 0; j < height; j++)
-               indexesOfColors[i][j] = palette.indexOf(color);
+                indexesOfColors[i][j] = palette.indexOf(color);
     }
 
-    public PaletteRasterImage(Color[][] pixels){
+    public PaletteRasterImage(Color[][] pixels) {
         this.width = pixels.length;
         this.height = pixels[0].length;
         palette = new ArrayList<>();
         indexesOfColors = new int[width][height];
-        for(int i = 0; i < this.width; i++)
-            for (int j = 0; j < this.height; j++){
-                if (!palette.contains((pixels[i][j]))){
+        for (int i = 0; i < this.width; i++)
+            for (int j = 0; j < this.height; j++) {
+                if (!palette.contains((pixels[i][j]))) {
                     indexesOfColors[i][j] = palette.indexOf(pixels[i][j]);
                 }
-
+            }
 
             }
 
 
-    public void creatRepresentation(){
-        palette = new ArrayList<>();
-        palette.add(Color.WHITE);
-        for(int i = 0; i < this.width; i++)
-            for (int j = 0; j < this.height; j++){
-                if (!palette.contains(palette.get(indexesOfColors[i][j]))){
-                    indexesOfColors[i][j] = palette.indexOf(Color.WHITE);
+        public void creatRepresentation () {
+            palette = new ArrayList<>();
+            palette.add(Color.WHITE);
+            for (int i = 0; i < this.width; i++)
+                for (int j = 0; j < this.height; j++) {
+                    if (!palette.contains(palette.get(indexesOfColors[i][j]))) {
+                        indexesOfColors[i][j] = palette.indexOf(Color.WHITE);
+                    }
                 }
-            }
+        }
+
+        @Override
+        public Color getPixelColor ( int x, int y){
+            return palette.get(indexesOfColors[x][y]);
+        }
+
+        @Override
+        public int getWidth () {
+            return this.width;
+
+        }
+
+        @Override
+        public int getHeight () {
+            return this.height;
+        }
+
+        public void setWidth ( int width){
+            this.width = width;
+        }
+
+        public void setHeight ( int height){
+            this.height = height;
+        }
+
+        public void setPixelsColor (Color[][]pixels){
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++) {
+                    if (!palette.contains(pixels[i][j]))
+                        palette.add(pixels[i][j]);
+                    indexesOfColors[i][j] = palette.indexOf(pixels[i][j]);
+                }
+        }
     }
 
-    @Override
-    public Color getPixelColor(int x, int y) {
-        return palette.add(indexesOfColors[x][y]);
-    }
-
-    @Override
-    public int getWidth() {
-        return this.width;
-
-    }
-
-    @Override
-    public int getHeight() {
-        return this.height;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setPixelsColor(Color[][] pixels){
-        for (int i = 0; i < width; i++)
-            for (int j = 0; j < height; j++) {
-                if (!palette.contains(pixels[i][j]))
-                    palette.add(pixels[i][j]);
-                indexesOfColors[i][j] = palette.indexOf(pixels[i][j]);
-            }
-    }
-}
--- src/main/java/viewer/Display.java
+++ src/main/java/viewer/Display.java
@@ -24,11 +24,11 @@ public class Display implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.BRUTE);
+        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.PALETTE);
 
-        imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.BRUTE);
+        imageFactory = new RasterUniformImageFactory(200, 200, Color.RED, RasterImageType.PALETTE);
 
-        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.BRUTE);
+        imageFactory = new RasterFlagFactory(900, 600, Color.BLUE, Color.WHITE, Color.RED, RasterImageType.PALETTE);
 
 
         this.image = imageFactory.makeImage();
